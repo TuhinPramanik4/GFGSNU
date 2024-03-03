73bfd338519bfa825c9f8bf56d7034fd86ec4f06
Tuhin Pramanik <156042548+TuhinPramanik4@users.noreply.github.com>
2024-03-03 09:02:46 +0530

    Add files via upload

---
+++ #Day_24.c
@@ -0,0 +1,12 @@
+int majorityElement(int* nums, int numsSize) {
+    int candidate, count = 0;
+    for (int i = 0; i < numsSize; i++) {
+        if (count == 0) {
+            candidate = nums[i];
+            count = 1;
+        } else if (nums[i] == candidate) {
+            count++;
+        } else {
+            count--;
+        }
+    }
